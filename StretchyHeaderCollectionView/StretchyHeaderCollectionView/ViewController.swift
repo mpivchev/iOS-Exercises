8d7278f94021f2d87f43035afdc4717f03b4212f
Milen Pivchev <milen.pivchev@gmail.com>
2020-04-19 12:05:46 +0200

    initial commit

---
+++ StretchyHeaderCollectionView/StretchyHeaderCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StretchyHeaderCollectionView
+//
+//  Created by Milen Pivchev on 19.04.20.
+//  Copyright © 2020 Milen Pivchev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
