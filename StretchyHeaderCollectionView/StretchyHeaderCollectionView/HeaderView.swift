a9d54496ba5807cf64693eb8885e6ff817361d78
Milen Pivchev <milen.pivchev@gmail.com>
2020-04-26 16:01:08 +0200

    added gradient and text to header

--- StretchyHeaderCollectionView/StretchyHeaderCollectionView/HeaderView.swift
+++ StretchyHeaderCollectionView/StretchyHeaderCollectionView/HeaderView.swift
@@ -11,20 +11,24 @@ import UIKit
 class HeaderView: UICollectionReusableView {
     var blurAnimator: UIViewPropertyAnimator!
     
-    let imageView : UIImageView = {
-        let iv = UIImageView(image: #imageLiteral(resourceName: "958580"))
-        iv.contentMode = .scaleAspectFill
-        return iv
-    }()
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .red
+        
+        initImageView()
+        initBlur()
+        initGradientLayer()
+        initText()
+    }
+    
+    private func initImageView() {
+        let imageView : UIImageView = {
+            let iv = UIImageView(image: #imageLiteral(resourceName: "958580"))
+            iv.contentMode = .scaleAspectFill
+            return iv
+        }()
         
         addSubview(imageView)
         imageView.fillSuperview()
-        
-        initBlur()
     }
     
     private func initBlur() {
@@ -38,6 +42,37 @@ class HeaderView: UICollectionReusableView {
         })
         
         blurAnimator.pausesOnCompletion = true // enables the effect to remain active after app goes in the background
+    }
+    
+    private func initGradientLayer() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.5, 1]
+        
+        let gradientContainerView = UIView()
+        addSubview(gradientContainerView)
+        gradientContainerView.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
+        gradientContainerView.layer.addSublayer(gradientLayer)
+        
+        gradientLayer.frame = bounds
+        gradientLayer.frame.origin.y -= bounds.height
+    }
+    
+    private func initText() {
+        let title = UILabel()
+        title.text = "Test title"
+        title.font = .systemFont(ofSize: 24, weight: .bold)
+        title.textColor = .white
+        
+        let subtitle = UILabel()
+        subtitle.text = "Test subtitle"
+        subtitle.font = .systemFont(ofSize: 18)
+        subtitle.textColor = .white
+        
+        let stackView = UIStackView(arrangedSubviews: [title, subtitle])
+        stackView.axis = .vertical
+        addSubview(stackView)
+        stackView.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 16, bottom: 16, right: 16))
     }
     
     required init?(coder: NSCoder) {
