f702e9a24e5a702601a55d94d9431cdb70eb402f
Milen Pivchev <milen.pivchev@gmail.com>
2020-05-15 13:56:24 +0200

    Fixed header inside tableview

---
+++ Storyboards/TabbedApp/Sources/Concerns/PhotoDetail/Components/HeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  HeaderView.swift
+//  TabbedApp
+//
+//  Created by Milen Pivchev on 02.05.20.
+//  Copyright © 2020 Milen Pivchev. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Storyboards/TabbedApp/Sources/Extensions/Extensions+UIView.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions+UIView.swift
+//  TabbedApp
+//
+//  Created by Milen Pivchev on 03.05.20.
+//  Copyright © 2020 Milen Pivchev. All rights reserved.
+//
+
+import Foundation
